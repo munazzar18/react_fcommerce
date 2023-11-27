afde47dca714f22e17ebb587074af6f1c6d5efa3
Munazzar Khan <gex.18@hotmail.com>
2023-11-27 22:53:38 +0500

    landing page started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,34 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="font-mono text-5xl">Comming Soon...</h1>
+    <main>
+      <div className="flex min-h-screen flex-col items-center justify-between w-full relative ">
+        <div className="absolute inset-0 z-0">
+          <img
+            src="../../bg.jpg"
+            className="w-full h-full object-cover"
+            alt="Background Image"
+          />
+        </div>
+        <div className="relative z-0 flex place-items-center ">
+          <div className="relative flex flex-wrap justify-center items-center mb-8 bg-lime-500">
+            <h1 className="font-mono text-5xl">Welcome to Fcommerce Store!</h1>
+          </div>
+          <div className="relative top-52 inset-x-[-28rem] z-10 w-96 bg-yellow-800 ml-60">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum
+            tempore minus, assumenda veniam saepe nesciunt quo temporibus.
+            Recusandae dignissimos quisquam maiores optio soluta ipsam. Soluta
+            nulla, ipsa excepturi corrupti quam dolorum ipsam pariatur earum
+            praesentium illo quibusdam minus dicta eligendi nobis delectus harum
+            consectetur vero saepe amet id. Optio debitis, voluptates
+            consequatur repellat architecto, repellendus numquam exercitationem
+            ipsum ad sint distinctio doloremque voluptas ut illum doloribus
+            nostrum cupiditate laborum. Fugiat?
+            <div className="m-4 ml-64 mt-2">
+              <button className="p-2">Browse</button>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
